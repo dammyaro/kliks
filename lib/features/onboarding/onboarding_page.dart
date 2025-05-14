c96e3c38d23911cb831d98d8609899506104a6d2
Damilola Aromiwe <mac@Damilola.local>
2025-05-14 14:42:33 +0100

    Added post install script for Xcode cloud

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -107,7 +107,7 @@ class OnboardingPage extends StatelessWidget {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30), 
                       ),
-                      minimumSize: Size(screenWidth * 0.9, screenHeight * 0.07), 
+                      minimumSize: Size(screenWidth * 0.9, screenHeight * 0.06), 
                     ),
                     child: Text(
                       'Continue as Guest',
